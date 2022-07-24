d2696f527992f8f785115db49c464aaf494799a1
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2022-07-24 16:29:04 +0530

    Create MaxSparseTable.java

---
+++ MaxSparseTable.java
@@ -0,0 +1,30 @@
+class MaxSparseTable{
+    int n;
+    int[] logs;
+    int[] arr;
+    int[][] table;
+    MaxSparseTable(int[] a){
+        this.n = a.length;
+        this.arr = new int[n];
+        for(int i=0;i<a.length;i++) arr[i] = a[i];
+        int maxPowerOfTwo = Math.max(1,(int)Math.ceil(Math.log(n)/Math.log(2)));
+        this.logs = new int[n+1];
+        computeLogs();
+        this.table = new int[maxPowerOfTwo][n];
+        for(int i=0;i<=logs[n];i++){
+            int curLen = 1<<i;
+            for(int j=0;j<=n-curLen;j++){
+                if(curLen==1) table[i][j] = arr[j];
+                else table[i][j] = Math.max(table[i-1][j],table[i-1][j+(curLen/2)]);
+            }
+        }
+    }
+    private void computeLogs(){
+        for(int i=2;i<=n;i++) logs[i] = logs[i/2]+1;
+    }
+    public int getMaximum(int left, int right){
+        int p = logs[right-left+1];
+        int pLen = 1 << p;
+        return Math.max(table[p][left],table[p][right-pLen+1]);
+    }
+}
